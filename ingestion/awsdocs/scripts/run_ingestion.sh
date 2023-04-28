3266d9bb376beeb7103a3b97f32c612f3c064e66
Malte Reimann <malterei@amazon.ch>
2023-04-28 11:44:56 +0200

    Update to python3.8 and farm-haystack 1.15.0 to escape dependency hell https://github.com/deepset-ai/haystack/issues/3012

--- ingestion/awsdocs/scripts/run_ingestion.sh
+++ ingestion/awsdocs/scripts/run_ingestion.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && 
 
 echo "script_dir MAIN: $SCRIPT_DIR"
 bash $SCRIPT_DIR/clone_awsdocs.sh $1
-python3 $SCRIPT_DIR/../src/ingest.py ./
+python3.8 $SCRIPT_DIR/../src/ingest.py ./
